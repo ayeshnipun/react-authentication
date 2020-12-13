e4b1606683bcd099d626a204200c42cdf4d4787b
ayeshN <ayesh@ascentic.se>
2020-12-13 17:20:25 +0530

    final touch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import AuthProvider from './providers/auth-provider';
-
+import logo from './logo.svg';
 import './App.css';
 import Login from "./pages/login";
 
 const App = () => {
   return (
+	<div className="App">
+	<header className="App-header">
+	  <img src={logo} className="App-logo" alt="logo" />
+	  
 	  <Login/>
+	</header>
+  </div>
   );
 };
 
